e14a4ab2b328d22cf2bbd2bd553604650c749427
James Robert <jiaaro@gmail.com>
2014-03-18 21:01:57 -0400

    keep track of subscribe clicks

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function() {
-  $(".show-notes a").attr("target", "_blank");  
+  $(".show-notes a").attr("target", "_blank");
+
+  $(".subscribe-btn").click(function() {
+    ga('send', 'event', 'Subscribe button', 'click', 'itunes');
+  });
+  $(".rss-subscribe").click(function() {
+    ga('send', 'event', 'Subscribe button', 'click', 'rss');
+  });
 });
